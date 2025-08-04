639aa131844610b551bb931857bf579c4c18620f
sceptyre <abdulla@oblongata.dev>
2025-08-03 23:58:49 -0400

    fix: openai mappings

--- agnets/backends/openai.py
+++ agnets/backends/openai.py
@@ -1,9 +1,77 @@
 from openai import OpenAI
+from openai.types.chat.chat_completion_message import ChatCompletionMessage
 from openai.types.chat.chat_completion_tool_param import ChatCompletionToolParam
 from openai.types.shared_params.function_definition import FunctionDefinition
 
 from ..types.backend import Backend
-from ..types.message import Message
+from ..types.message import Message, MessageComponent, MessageToolCallComponent
+
+from typing import Dict
+
+import mcp.types
+
+import json
+
+def _map_to_openai_message(message: Message) -> Dict:
+    msg = {
+        "role": message.role,
+        'content': '',
+        'thinking': None,
+        'tool_calls': []
+    }
+
+    for component in message.components:
+        if component.type == 'message':
+            msg['content'] = component.content
+            continue
+
+        if component.type == 'thinking':
+            msg['thinking'] = component.content
+            continue
+
+        if component.type == 'tool_call':
+            msg['tool_calls'].append({
+                'id': component._meta.get('tool_call_id'),
+                'name': component.content.params.name,
+                'arguments': component.content.params.arguments
+            })
+            continue
+
+    return msg
+
+def _map_to_openai_tool(tool: mcp.types.Tool) -> ChatCompletionToolParam:
+    return ChatCompletionToolParam(
+        type='function',
+        function=FunctionDefinition(
+            name=tool.name,
+            description=tool.description,
+            parameters=tool.parameters
+        )
+    )
+
+def _map_from_openai_message(message: ChatCompletionMessage) -> Message:
+    msg = Message(role=message.role, components=[])
+
+    if message.content:
+        msg.components.append(MessageComponent(
+            type='message',
+            content=message.content
+        ))
+
+    for tool_call in message.tool_calls or []:
+        msg.components.append(MessageToolCallComponent(
+            type='tool_call',
+            content=mcp.types.CallToolRequest(
+                method='tools/call',
+                params=mcp.types.CallToolRequestParams(
+                    _meta={'tool_call_id': tool_call.id},
+                    name=tool_call.function.name,
+                    arguments=json.loads(tool_call.function.arguments)
+                )
+            )
+        ))
+
+    return msg
 
 class OpenAICompatibleBackend(Backend):
     def model_post_init(self, ctx):
@@ -23,24 +91,11 @@ class OpenAICompatibleBackend(Backend):
         ]
 
         for message in messages:
-            mapped_messages.append({
-                'role': message.role,
-                'content': message.message_content
-            })
+            mapped_messages.append(_map_to_openai_message(message))
 
         tools_mapped = []
         for tool in tools:
-            print(tool)
-            tools_mapped.append(
-                ChatCompletionToolParam(
-                    type='function',
-                    function=FunctionDefinition(
-                        name=tool.name,
-                        description=tool.description,
-                        parameters=tool.parameters
-                    )
-                )
-            )
+            tools_mapped.append(_map_to_openai_tool(tool))
             
 
         response = self._client.chat.completions.create(
@@ -48,8 +103,11 @@ class OpenAICompatibleBackend(Backend):
             messages=mapped_messages,
             tools=tools_mapped
         )
+        print(response)
 
         output = []
+
+        return _map_from_openai_message(response.choices[0].message)
         
         if response.choices[0].message.reasoning:
             output.append(Message(
