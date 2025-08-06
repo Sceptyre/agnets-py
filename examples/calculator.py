e31abdd52c8192c5f11f8ebd211907524bb6f117
sceptyre <abdulla@oblongata.dev>
2025-08-05 22:40:53 -0400

    fix: small tweak to example sys prompt

--- examples/calculator.py
+++ examples/calculator.py
@@ -4,7 +4,7 @@ from agnets.backends.openai import OpenAICompatibleBackend
 agnet = Agnet(
     config=Config(
         model_name="z-ai/glm-4-32b",
-        system_prompt="ALWAYS use tools available to respond to your fellow agent."
+        system_prompt="ALWAYS use tools available to respond to your fellow agent. Your response MUST be in a tool call."
     ),
     backend=OpenAICompatibleBackend()
 )
