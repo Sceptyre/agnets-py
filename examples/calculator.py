73bb4cf18bcc066cbd0b0dbfb0138b62481cdffc
Abdellah Fytahi <61129849+Sceptyre@users.noreply.github.com>
2025-08-13 02:33:33 +0000

    feat: fleet is v2 agnets only (#5)
    * feat: init litellm backend
    
    * feat: more mappings
    
    * feat: api config for litellm
    
    * feat: update openai example
    
    * feat: thinking mapping
    
    * build: hatch vcs
    
    * feat: v2 and more litellm
    
    * chore: type hints and cleanup
    
    * feat: lockfile litellm
    
    * feat: building out fleet further with litellm
    
    ---------
    
    Co-authored-by: Sceptyre <adam@swiftmanaged.com>
    Co-authored-by: sceptyre <abdulla@oblongata.dev>

--- examples/calculator.py
+++ examples/calculator.py
@@ -1,15 +1,18 @@
 from agnets import Agnet, Config
+from agnets.agentv2 import Agent
 from agnets.backends.openai import OpenAICompatibleBackend
+# from agnets.backends.ollama import OllamaBackend
+from agnets.backends.litellm import LiteLLMBackend
 
-agnet = Agnet(
+agnet = Agent(
     config=Config(
-        model_name="z-ai/glm-4-32b",
+        model_name="openai/openai/gpt-oss-20b",
         system_prompt="""
 ALWAYS use tools available to respond to your fellow agent. 
 Your response MUST be in a tool call.
+Present to the user your answer with the `respond_to_user` tool.
 """
     ),
-    backend=OpenAICompatibleBackend()
 )
 
 @agnet.add_tool
@@ -20,16 +23,16 @@ def add(a: int, b: int) -> int:
 def multiply(a: int, b: int) -> int: 
     return a * b
 
-# @agnet.add_tool
-# def respond_to_user(response: str):
-#     """
-#     Responds to user
-#     """
-#     print(response)
-
-#     return True
-
 if __name__ == "__main__":
+    @agnet.add_tool
+    def respond_to_user(response: str):
+        """
+        Responds to user
+        """
+        print(response)
+
+        return True
+
     user_input = input(">>> ")
 
     agnet.invoke(user_input, stop_on=['respond_to_user'])
