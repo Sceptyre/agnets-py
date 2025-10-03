effa40f6c10d7ba36c88f263635a8b3fd34f6562
Abdellah Fytahi <61129849+Sceptyre@users.noreply.github.com>
2025-10-02 21:34:44 -0400

    feat: support async tool calls and fastmcp tool manager (#6)
    * feat: init litellm backend
    
    * feat: more mappings
    
    * feat: api config for litellm
    
    * feat: update openai example
    
    * feat: thinking mapping
    
    * build: hatch vcs
    
    * feat: v2 and more litellm
    
    * chore: type hints and cleanup
    
    * feat: lockfile litellm
    
    * feat: building out fleet further with litellm
    
    * feat: add asyncio
    
    * feat: temp solution for async tool functions
    
    * feat: implementing the basic-memory fastmcp tool manager
    
    ---------
    
    Co-authored-by: Sceptyre <adam@swiftmanaged.com>
    Co-authored-by: sceptyre <abdulla@oblongata.dev>

--- examples/organization.py
+++ examples/organization.py
@@ -1,21 +1,33 @@
+"""
+NOTE: Requires basic-memory to be install via
+uv pip install basic-memory
+"""
+
 from agnets.agentv2 import Agent
 from agnets.config import Config
 from agnets.utils import new_simple_litellm_router
 from agnets.fleet import Fleet
-# import logging
+import logging
 
 # logging.basicConfig(level=logging.DEBUG)
+
+
+from basic_memory.mcp.server import mcp as basic_memory_mcp
+
+import basic_memory.mcp.tools
+
+# print(basic_memory_mcp._tool_manager._tools)
 
 f = Fleet()
 
 def new_simple_agent(agent_name: str, prompt: str) -> Agent:
     r = new_simple_litellm_router(
         provider_name='openai',
-        model_name='openai/gpt-oss-20b'
+        model_name='openai/gpt-oss-120b'
     )
 
     c = Config(
-        model_name='openai/gpt-oss-20b',
+        model_name='openai/gpt-oss-120b',
         system_prompt=f"""
 You are {agent_name}
 
@@ -41,16 +53,23 @@ agents = [
 ]
 
 
-for a in agents:
-    agent = new_simple_agent(a[0], a[1])
+def main():
+    for a in agents:
+        agent = new_simple_agent(a[0], a[1])
 
-    f.add_agent(a[0], agent, a[2])
+        f.add_agent(a[0], agent, a[2])
 
-CEO_AGENT = f.agents.get('Chief Executive Officer')
-@CEO_AGENT.add_tool
-def respond_to_user(response: str):
-    return response
+    CEO_AGENT = f.agents.get('Chief Executive Officer')
+    @CEO_AGENT.add_tool
+    def respond_to_user(response: str):
+        return response
 
-response = CEO_AGENT.invoke('We need to establish a security strategy for any new products we release', stop_on=['respond_to_user'])
-response_content = response[-1]['content']
-print(response_content)
+    for k, v in basic_memory_mcp._tool_manager._tools.items():
+        CEO_AGENT.set_tool(k, v)
+
+    response = CEO_AGENT.invoke('We need to establish a security strategy for any new products we release. Use your memory', stop_on=['respond_to_user'])
+    response_content = response[-1]['content']
+    print(response_content)
+
+if __name__ == '__main__':
+    main()
